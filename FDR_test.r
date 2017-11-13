5e0fc6d6c41abd65cd85216a33ea94674f09c60d
ming623 <31195671+ming623@users.noreply.github.com>
2017-11-14 04:21:39 +0800

    Add files via upload

--- FDR_test.r
+++ FDR_test.r
@@ -5,4 +5,5 @@ FDR <- read.xlsx(file = "FDR_test.xlsx", sheetIndex = 1,
 FDR <- FDR[order(FDR$P.value),]
 FDR$FDR_value <- p.adjust(FDR$P.value, method = "fdr")
 FDR$Benferroni_value <- p.adjust(FDR$P.value, method = "bonferroni")
-
+write.xlsx(FDR,"FDR_result.xlsx",sheetName = "Sheet1",
+           col.names = TRUE, row.names = TRUE, append = FALSE,showNA = TRUE)
